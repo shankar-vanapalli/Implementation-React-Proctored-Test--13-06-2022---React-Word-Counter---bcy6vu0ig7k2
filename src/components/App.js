25306f97d28c14f1b5c706bb7b306c550c433d21
Bhavani Shankar Vanapalli <40032387+shankar067@users.noreply.github.com>
2022-06-13 21:38:23 +0530

    Commited Changes!!!

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import '../styles/App.css';
+import React from "react";
+import "../styles/App.css";
+import Ground from "./Ground";
 const App = () => {
-
-
   return (
     <div id="main">
+      <Ground />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
---
+++ src/components/Ground.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+
+const Ground = () => {
+  const [limit, setLimit] = useState(50);
+  const [text, setText] = useState("");
+  const [range, setRange] = useState("16");
+
+  const limitHandler = (e) => {
+    setLimit(e.target.value);
+  };
+  const textHandler = (e) => {
+    if (text.length < limit) {
+      setText(e.target.value);
+    }
+  };
+  const rangeHandler = (e) => {
+    setRange(e.target.value);
+  };
+  // const style = {
+  //   height: `${range}px`
+  // };
+  return (
+    <div>
+      <div>
+        <input
+          type="range"
+          onInput={rangeHandler}
+          defaultValue={range}
+          id="fontSize-input"
+          min="16"
+          max="32"
+        />
+      </div>
+      <div>
+        <input
+          type="number"
+          id="char-limit-input"
+          value={limit}
+          onChange={limitHandler}
+        />
+      </div>
+      <div>
+        <textarea
+          style={{
+            height: `${range}px`
+          }}
+          placeholder="Enter words here"
+          onChange={textHandler}
+          value={text}
+        />
+      </div>
+      <div id="word-counter">
+        Total number of words written:{" "}
+        {text.length !== 0
+          ? text.split(" ").filter(function (n) {
+              return n !== "";
+            }).length
+          : 0}
+      </div>
+      <div id="char-counter">
+        Total number of characters used: {text.length !== 0 ? text.length : 0}
+      </div>
+    </div>
+  );
+};
+
+export default Ground;
